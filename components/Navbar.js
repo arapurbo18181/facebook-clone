a1b97f607f03168b8cbc8f349fb0120b02118a3e
MD Abdul Zabber Islam Apurbo <md.abdul.zabber.islam.apurbo@g.bracu.ac.bd>
2022-10-27 18:47:21 +0600

    navbar refactored

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,69 @@
 import Link from "next/link";
-import React from "react";
-import {
-  BsShop,
-  BsMessenger,
-} from "react-icons/bs";
+import React, { useRef, useState } from "react";
+import { BsShop, BsMessenger } from "react-icons/bs";
 import { AiOutlineHome } from "react-icons/ai";
 import { TbGridDots, TbDeviceGamepad } from "react-icons/tb";
-import { MdNotificationsActive, MdOutlineGroups, MdOutlineOndemandVideo } from "react-icons/md";
+import {
+  MdNotificationsActive,
+  MdOutlineGroups,
+  MdOutlineOndemandVideo,
+} from "react-icons/md";
 import Image from "next/image";
 
 const Navbar = () => {
+  const clickToggle = () => {
+    if (!ref.current.classList.contains("border-b-4")) {
+      ref.current.classList.add("border-b-4");
+      ref2.current.classList.remove("border-b-4");
+      ref3.current.classList.remove("border-b-4");
+      ref4.current.classList.remove("border-b-4");
+      ref5.current.classList.remove("border-b-4");
+    } 
+    
+    
+    
+  }
+  const clickToggle2 = () =>{
+    if (!ref2.current.classList.contains("border-b-4")) {
+      ref.current.classList.remove("border-b-4");
+      ref2.current.classList.add("border-b-4");
+      ref3.current.classList.remove("border-b-4");
+      ref4.current.classList.remove("border-b-4");
+      ref5.current.classList.remove("border-b-4");
+    }
+  }
+  const clickToggle3 = () =>{
+    if (!ref3.current.classList.contains("border-b-4")) {
+      ref.current.classList.remove("border-b-4");
+      ref2.current.classList.remove("border-b-4");
+      ref3.current.classList.add("border-b-4");
+      ref4.current.classList.remove("border-b-4");
+      ref5.current.classList.remove("border-b-4");
+    }
+  }
+  const clickToggle4 = () =>{
+    if (!ref4.current.classList.contains("border-b-4")) {
+      ref.current.classList.remove("border-b-4");
+      ref2.current.classList.remove("border-b-4");
+      ref3.current.classList.remove("border-b-4");
+      ref4.current.classList.add("border-b-4");
+      ref5.current.classList.remove("border-b-4");
+    }
+  }
+  const clickToggle5 = () =>{
+    if (!ref5.current.classList.contains("border-b-4")) {
+      ref.current.classList.remove("border-b-4");
+      ref2.current.classList.remove("border-b-4");
+      ref3.current.classList.remove("border-b-4");
+      ref4.current.classList.remove("border-b-4");
+      ref5.current.classList.add("border-b-4");
+    }
+  }
+  const ref = useRef();
+  const ref2 = useRef();
+  const ref3 = useRef();
+  const ref4 = useRef();
+  const ref5 = useRef();
   return (
     <div className="bg-[#242526]">
       <div className="flex justify-between items-center mx-5">
@@ -18,24 +72,46 @@ const Navbar = () => {
             <Image src={"/facebook-logo.png"} alt="" height={45} width={45} />
           </div>
           <div>
-            <input className="bg-[#3a3b3c] py-2 px-4 mx-2 rounded-full text-white w-64" type="text" placeholder="Search Facebook" />
+            <input
+              className="bg-[#3a3b3c] py-2 px-4 mx-2 rounded-full text-white w-64"
+              type="text"
+              placeholder="Search Facebook"
+            />
           </div>
         </div>
         <div className="my-1">
           <ul className="flex text-[#b0b3b8] text-3xl">
-            <Link href={"/"} className="hover:bg-[#3a3b3c] w-28 h-14 flex justify-center items-center rounded-lg" title="Home">
-              <AiOutlineHome />
-            </Link>
-            <Link href={"/"} className="hover:bg-[#3a3b3c] w-28 h-14 flex justify-center items-center rounded-lg" title="Watch">
+            <div ref={ref} className="hover:bg-[#3a3b3c] w-28 h-14 flex justify-center items-center rounded-lg border-[#2374e1]" title="Home"  onClick={clickToggle}  >
+              <Link href={"/"}>
+                <AiOutlineHome />
+              </Link>
+            </div>
+            <Link ref={ref2}
+              href={"/"}
+              className="hover:bg-[#3a3b3c] w-28 h-14 flex justify-center items-center rounded-lg border-[#2374e1]"
+              title="Watch" onClick={clickToggle2} 
+            >
               <MdOutlineOndemandVideo />
             </Link>
-            <Link href={"/"} className="hover:bg-[#3a3b3c] w-28 h-14 flex justify-center items-center rounded-lg" title="Marketplace">
+            <Link ref={ref3}
+              href={"/"}
+              className="hover:bg-[#3a3b3c] w-28 h-14 flex justify-center items-center rounded-lg border-[#2374e1]"
+              title="Marketplace" onClick={clickToggle3} 
+            >
               <BsShop />
             </Link>
-            <Link href={"/"} className="hover:bg-[#3a3b3c] w-28 h-14 flex justify-center items-center rounded-lg" title="Groups">
-              <MdOutlineGroups/>
+            <Link ref={ref4}
+              href={"/"}
+              className="hover:bg-[#3a3b3c] w-28 h-14 flex justify-center items-center rounded-lg border-[#2374e1]" 
+              title="Groups" onClick={clickToggle4} 
+            >
+              <MdOutlineGroups />
             </Link>
-            <Link href={"/"} className="hover:bg-[#3a3b3c] w-28 h-14 flex justify-center items-center rounded-lg" title="Gaming">
+            <Link ref={ref5}
+              href={"/"}
+              className="hover:bg-[#3a3b3c] w-28 h-14 flex justify-center items-center rounded-lg border-[#2374e1]"
+              title="Gaming" onClick={clickToggle5} 
+            >
               <TbDeviceGamepad />
             </Link>
           </ul>
@@ -50,8 +126,14 @@ const Navbar = () => {
           <div className="w-11 h-11 flex justify-center items-center rounded-full bg-[#3a3b3c] hover:bg-[#4e4f50] cursor-pointer">
             <MdNotificationsActive />
           </div>
-          <div className="w-11 h-11 rounded-full overflow-hidden">
-            <Image src={"/man2.webp"} alt="" height={70} width={70} className="" />
+          <div className="w-11 h-11 rounded-full overflow-hidden cursor-pointer">
+            <Image
+              src={"/man2.webp"}
+              alt=""
+              height={70}
+              width={70}
+              className=""
+            />
           </div>
         </div>
       </div>
